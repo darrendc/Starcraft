651cc955f816cb3a04c7d51a49779f727117395c
Darren DCosta <ddcosta@gmail.com>
2019-06-07 23:46:13 -0400

    debugged sessions control with link_to forlogging out of a session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,23 +4,17 @@ class SessionsController < ApplicationController
     @player = Player.new
   end
 
-  def create
+  def create #login
     player = Player.find_by(name: params[:player][:name])
     if player && player.authenticate(params[:player][:password])
       redirect_to player_path(player)
     else
       redirect_to "/"
     end
-    # if params[:name].nil? || params[:name].empty?
-    #   redirect_to controller: "sessions", action: "new"
-    # else
-    #   session[:name] = params[:name]
-    #   redirect_to "/"
-    # end
   end
 
   def destroy
-    session.delete :name if session[:name]
+    session.delete :player_id
     redirect_to "/"
   end
 end
